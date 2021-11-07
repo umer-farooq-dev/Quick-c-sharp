cd83210957ce3d7989e025e12ad81c9cfcfe31c0
Umer Malik <ibsoft0786@gmail.com>
2021-11-07 23:03:00 +0500

    Tutorial 5 Updated

--- Tutorial 5.cs
+++ Tutorial 5.cs
@@ -33,3 +33,49 @@ void function(object param)
         //do something         
     }
 }
+
+/*
+as operator
+as operator checks if the type of the operands are convert-able or equal (as is done by is operator) and if it is, the result is a converted or boxed object (if the operand can be boxed into the target type, see boxing/unboxing). If the objects are not convert-able or box-able, the return is a null. Have a look at the example below to better understand the concept.
+*/
+
+Shape shp = new Shape(); 
+Vehicle veh = shp as Vehicle; // result is null, types are not convertable
+
+Circle cir = new Circle(); 
+Shape shp = cir; 
+Circle cir2 = shp as Circle;  //will be converted
+
+object[] objects = new object[2];
+objects[0] = "Umer";
+object[1] = new Shape();
+
+string str;
+for(int i=0; i&< objects.Length; i++)
+{
+    str = objects[i] as string;
+    if(str == null)
+        Console.WriteLine("can not be converted");
+    else
+        Console.WriteLine("{0}",str);
+}
+/*
+Output:
+Umer
+can not be converted
+*/
+
+/*
+
+Statements
+Statements in C# are just like in C++ except some additions of new statements and modifications in some statements.
+
+Followings are new statements:
+
+foreach
+For iteration of collections like arrays etc.
+*/
+//Example:
+
+foreach (string s in array)
+    Console.WriteLine(s);
