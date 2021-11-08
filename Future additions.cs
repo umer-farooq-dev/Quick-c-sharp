a25f60559f90fc2398e9e907b42049d5a436e315
Umer Malik <ibsoft0786@gmail.com>
2021-11-08 23:52:12 +0500

    Future additions

---
+++ Future additions.cs
@@ -0,0 +1,17 @@
+/*
+Future additions
+This article is just a quick overview of the C# language so that you can just become familiar with the language features. Although I have tried to discuss almost all the major concepts in C# in a brief and comprehensive way with code examples, yet I think there is lot much to be added and discussed.
+
+In future, I would like to add more commands and concepts not yet discussed, including events etc. I would also like to write for beginners, about Windows programming using C#.
+
+References:
+Our most commonly known MSDN
+Inside C# by Tom Archer
+A Programmer's Introduction to C# by Eric Gunnerson
+Beginning C# by Karli Watson
+Programming C# (O'Reilly)
+Modifications:
+June 12, 2003: By-Reference/In-Out Parameters- added ref keyword while calling a function with reference parameters
+June 20, 2003: Added a note for the optional parameters, corrected typo mistake of assignment operator in example of jagged array
+
+*/
