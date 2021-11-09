88cedfc60c112cac15a466ba99618208b990890f
Umer Malik <ibsoft0786@gmail.com>
2021-11-09 23:53:12 +0500

    Practice Program

---
+++ TaskProgram/TaskProgram/Anila.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProgram
+{
+    class Anila
+    {
+        //public void Fahrenheit()
+        //{
+        //    Console.WriteLine("___________/Welcome___________ \n");
+        //    int a;
+        //    float Fahrenheit_t, Centigrade_t;
+        //    Console.WriteLine("Enter the any Number and chek the temprature  in Fahrenhei");
+        //    Fahrenheit_t = Convert.ToSingle(Console.ReadLine());
+
+        //    Centigrade_t = (5.0F / 9.0F) * (Fahrenheit_t - 32);
+        //    Console.WriteLine("The Temperature in Centigrade Degree: " + Centigrade_t);
+
+        //}
+        //public void five_digit()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+        //    Console.WriteLine("Enter the any Number 5 number");
+        //    int a = Convert.ToInt32(Console.ReadLine());
+        //    int count = 0, b = 1, result;
+        //    int no = a;
+        //    while (no != 0)
+        //    {
+        //        no = no / 10;
+        //        count = count + 1;
+
+        //    }
+        //    for (int i = 1; i < count; i++)
+        //    {
+        //        b = b * 1;
+        //        b = b + 1;
+        //    }
+        //    result = a + b;
+        //    Console.WriteLine("Result is: " + result);
+
+        //}
+        //public void five_digit2()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+        //    int result, n1, n2, n3, n4, n5, d1, d2, d3, d4, d5;
+        //    Console.WriteLine("Enter the any  5 number");
+        //    int no = Convert.ToInt32(Console.ReadLine());
+        //    n1 = no / 10;
+        //    d5 = no % 10;
+        //    n2 = n1 / 10;
+        //    d4 = n1 % 10;
+        //    n3 = n2 / 10;
+        //    d3 = n2 % 10;
+        //    n4 = n3 / 10;
+        //    d2 = n3 % 10;
+        //    n5 = n4 / 10;
+        //    d1 = n4 % 10;
+        //    result = d1 + d2 + d3 + d4 + d5;
+
+
+        //    Console.WriteLine("Result is: " + result);
+
+        //}
+        //public void reverse()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+
+        //    int reverse = 0, reminder;
+        //    Console.WriteLine("Enter the any  5 number");
+        //    int no = Convert.ToInt32(Console.ReadLine());
+        //    while (no != 0)
+        //    {
+        //        reminder = no % 10;
+        //        reverse = reverse * 10 + reminder;
+        //        no /= 10;
+        //    }
+        //    Console.Write("Reversed Number: " + reverse);
+        //}
+        //public void Obtain_marks()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+        //    int result, n1, n2, n3, n4, n5, d1, d2, d3, d4;
+        //    Console.WriteLine("Enter the any  4 number");
+        //    int no = Convert.ToInt32(Console.ReadLine());
+        //    n1 = no / 10;
+        //    d4 = no % 10;
+        //    n2 = n1 / 10;
+        //    d3 = n1 % 10;
+        //    n3 = n2 / 10;
+        //    d2 = n2 % 10;
+        //    n4 = n3 / 10;
+        //    d1 = n3 % 10;
+        //    result = d1 + d4;
+        //    Console.WriteLine("Result is: " + result);
+
+        //}
+        //public void Town()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+        //    long men_p, women_p, total_l_p, men_l_p, no_l_m, no_l_w; //total_l_m_w;
+        //    long population = 80000;
+        //    men_p = population * (52 / 100);
+        //    women_p = population - men_p;
+        //    total_l_p = population * (48 / 100);
+        //    men_l_p = population * (35 / 100);
+        //    no_l_m = men_p - men_l_p;
+        //    no_l_w = women_p - (total_l_p - men_l_p);
+        //    Console.WriteLine("Total number of illiterate Men \n: " + no_l_m);
+        //    Console.WriteLine("Total number of illiterate Women : " + no_l_w);
+
+        //}
+        //public void Cshier()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+        //    int amount, ten, fifteen, hundred;
+        //    Console.WriteLine("Enter the amount you want to withraw \n: ");
+        //    amount = Convert.ToInt32(Console.ReadLine());
+        //    ten = amount / 10;
+        //    fifteen = amount / 15;
+        //    hundred = amount / 100;
+        //    Console.WriteLine("Total number of currency Notes  : " + ten, fifteen, hundred);
+
+        //}
+        //public void Selling()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+        //    int T_selling_price, T_profit, Cost_price, Cost_price1;
+        //    Console.WriteLine("Enter the total selling price of 15 item \n: ");
+        //    T_selling_price = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Enter the total profit on 15 item \n: ");
+        //    T_profit = Convert.ToInt32(Console.ReadLine());
+        //    Cost_price = T_profit - T_selling_price;
+        //    Cost_price1 = Cost_price / 15;
+        //    Console.WriteLine("cost price of one item : " + Cost_price1);
+
+        //}
+        //Missing question 9
+        //public void Store()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+
+        //    float n1,n2,n3,n4,n5, Sub_total = 0, Sale_Tex,Total;
+
+        //    Console.WriteLine("Enter the price 1 item \n: ");
+        //    n1 = Convert.ToSingle(Console.ReadLine());
+        //    Console.WriteLine("Enter the price 2 item \n: ");
+        //    n2 = Convert.ToSingle(Console.ReadLine());
+        //    Console.WriteLine("Enter the price 3 item \n: ");
+        //    n3 = Convert.ToSingle(Console.ReadLine());
+        //    Console.WriteLine("Enter the price 4 item \n: ");
+        //    n4 = Convert.ToSingle(Console.ReadLine());
+        //    Console.WriteLine("Enter the price 5 item \n: ");
+        //    n5 = Convert.ToSingle(Console.ReadLine());
+        //    Sub_total =n1+n2+n3+n4+n5;
+        //    Sale_Tex = Sub_total * 7;
+        //    Total = Sub_total + Sale_Tex;
+        //    Console.WriteLine("Subtotal of the sale :" + Sub_total);
+        //    Console.WriteLine("Amount of sales tax :" +Sale_Tex);
+        //    Console.WriteLine("Total :" + Total);
+
+
+
+        //}
+        //public void meal()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+        //    float meal, Percent_tip, Percent_sales_tax, Total_amount;
+        //    Console.WriteLine("Enter the charge for the meal \n: ");
+        //    meal = Convert.ToInt32(Console.ReadLine());
+        //    Percent_tip = meal * .18F;
+        //    Percent_sales_tax = meal * .7F;
+        //    Total_amount = meal + Percent_tip + Percent_sales_tax;
+        //    Console.WriteLine("The amount of percent tip  : " + Percent_tip);
+        //    Console.WriteLine("The amount of percent Sale tex  : " + Percent_sales_tax);
+        //    Console.WriteLine("The amount Meal Purchased  : " + Total_amount);
+
+        //}
+
+        //public void Selling()
+        //{
+
+        //    Console.WriteLine("___________/Welcome___________ \n");
+        //    float Total_sugar, Total_buuter, Total_flour;
+        //    Console.WriteLine("How many cookies he or she wants to make \n: ");
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    float COOKIES = 48;
+        //    float SUGAR = 1.5F;
+        //    float BUTTER = 1;
+        //    float FLOUR = 2.75F;
+        //    Total_sugar = n * SUGAR;
+        //    Total_buuter = n * BUTTER;
+        //    Total_flour = n * FLOUR;
+        //    Console.WriteLine("Totat cookies is : " + COOKIES);
+        //    Console.WriteLine("Totat Sugar is : " + Total_sugar);
+        //    Console.WriteLine("Total Butter is :" + Total_buuter);
+        //    Console.WriteLine("Total flour is :" + Total_flour);
+        //}
+
+        public void Student()
+        {
+
+            Console.WriteLine("___________/Welcome___________ \n");
+            float Total_student;
+            float Male_p, Females_p;
+            Console.WriteLine("Please enter the number of males in the class \n: ");
+            float males = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the number of females in the class \n: ");
+            float females = Convert.ToInt32(Console.ReadLine());
+            Total_student = males + females;
+            Male_p = (males / Total_student) * 100;
+            Females_p = (females / Total_student) * 100;
+
+            Console.WriteLine("Male Percentage is : " + Male_p);
+            Console.WriteLine("Females percentage is : " + Females_p);
+
+        }
+    }
+}
---
+++ TaskProgram/TaskProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Anila a = new Anila();
+            //a.Fahrenheit();
+            //a.five_digit();
+            //a.five_digit2();
+            //a.reverse();
+            //a.Obtain_marks();
+            //a.Town();
+            //a.Cshier();
+            //a.Selling();
+            //a.Store();
+            //a.meal();
+            //a.Selling();
+            a.Student();
+            Console.ReadKey();
+        }
+    }
+}
