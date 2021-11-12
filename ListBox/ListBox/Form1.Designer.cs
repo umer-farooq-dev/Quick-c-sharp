a119c71a6a2ec3bdee5a8958586f83888cdac349
Umer Malik <ibsoft0786@gmail.com>
2021-11-12 23:39:54 +0500

    Practice ListProject

---
+++ ListBox/ListBox/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnPopulate = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnRightAll = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(137, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(278, 77);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(137, 264);
+            this.listBox2.TabIndex = 1;
+            // 
+            // btnPopulate
+            // 
+            this.btnPopulate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnPopulate.Font = new System.Drawing.Font("Century Schoolbook", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPopulate.Location = new System.Drawing.Point(175, 77);
+            this.btnPopulate.Name = "btnPopulate";
+            this.btnPopulate.Size = new System.Drawing.Size(75, 42);
+            this.btnPopulate.TabIndex = 2;
+            this.btnPopulate.Text = ">";
+            this.btnPopulate.UseVisualStyleBackColor = false;
+            this.btnPopulate.Click += new System.EventHandler(this.btnPopulate_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnRight.Font = new System.Drawing.Font("Century Schoolbook", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRight.Location = new System.Drawing.Point(169, 143);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(85, 42);
+            this.btnRight.TabIndex = 3;
+            this.btnRight.Text = ">>>";
+            this.btnRight.UseVisualStyleBackColor = false;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnRightAll
+            // 
+            this.btnRightAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnRightAll.Font = new System.Drawing.Font("Century Schoolbook", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRightAll.Location = new System.Drawing.Point(175, 209);
+            this.btnRightAll.Name = "btnRightAll";
+            this.btnRightAll.Size = new System.Drawing.Size(75, 42);
+            this.btnRightAll.TabIndex = 4;
+            this.btnRightAll.Text = "<";
+            this.btnRightAll.UseVisualStyleBackColor = false;
+            this.btnRightAll.Click += new System.EventHandler(this.btnRightAll_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnClear.Font = new System.Drawing.Font("Century Schoolbook", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(169, 284);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(85, 42);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "<<<";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ListLeft";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(273, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ListRight";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(427, 399);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRightAll);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnPopulate);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "MahnoorTariq-17-ARID-1977 FALL 2020 ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnPopulate;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnRightAll;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ListBox/ListBox/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPopulate_Click(object sender, EventArgs e)
+        {
+            
+            listBox1.Items.Add("C");
+
+            listBox1.Items.Add("C++");
+
+            listBox1.Items.Add("Java");
+
+            listBox1.Items.Add("C#");
+
+            listBox1.Items.Add("Database");
+
+            listBox1.Items.Add("Networking");
+
+            listBox1.Items.Add("Operating System");
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            //ii) When user click btnRight, if there is an item selected (otherwise give Message to user that no item is selected) in LstLeft, it would move from LstLeft to LstRight.
+            if (listBox1.SelectedIndex == -1) 
+
+                MessageBox.Show("Please select the item to move");
+
+            else
+
+            {
+
+                while (listBox1.SelectedItems.Count > 0) 
+
+                {
+
+                    string itemList1 = (string)listBox1.SelectedItems[0];
+
+                    listBox2.Items.Add(itemList1);
+
+                    listBox1.Items.Remove(itemList1);
+
+                }
+
+            }
+        }
+
+        private void btnRightAll_Click(object sender, EventArgs e)
+        {
+            //iii) When user click btnRightAll, if there are one or more items are selected in LstLeft, all will be moved from LstLeft to LstRight.
+            while (listBox1.SelectedItems.Count > 0) 
+
+                {
+
+                    string itemList1 = (string)listBox1.SelectedItems[0];
+
+                    listBox2.Items.Add(itemList1);
+
+                    listBox1.Items.Remove(itemList1);
+
+                }
+
+            }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //iv) When User click btnClear, write a code that will empty/clear both lists.
+         
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+    }
+        }
+    
+
